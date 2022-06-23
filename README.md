ba97891ea672ba55031ee4a0992e53230a2213e2
ArumMelton <103224098+ArumMelton@users.noreply.github.com>
2022-06-22 22:27:45 -0400

    Updated and completed ReadMe, added a screenshot of functional app, and cleaned up as well as commented out the Javascript code.

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # pass-the-word
-Password generator
+
+Password Generator (Coded by Arum Melton)
+
+User story:
+"AS AN employee with access to sensitive data
+I WANT to randomly generate a password that meets certain criteria
+SO THAT I can create a strong password that provides greater security"
+
+In order to make passwords more secure, it is helpful to use a combination of upper and lowercase letters, numbers, and symbols.
+This application will give you a random assortment of characters for your password, based on certain parameters that are specified by the user via prompts.
+
+ACCEPTANCE CRITERIA:
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page.
+
+Deployed Application: https://arummelton.github.io/pass-the-word/
+Github Repo: https://github.com/ArumMelton/pass-the-word
--- assets/genscript.js
+++ assets/genscript.js
@@ -1,25 +1,39 @@
+// Declare variables for button and possible characters. //
+
 var generateBtn = document.querySelector("#generate");
 var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
 var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var charsNumbers = '0123456789';
 var charsSymbols = '+-_=/;><)(*&^%$#@!&{}[]';
 
+// Function for generating password. //
 
 function generatePassword() {
 
+  // Character count conditional statement first. If input is unsatisfactory, asks again. //
+
     var promptCount = window.prompt('How many characters would you like your password to contain?')
-    if (promptCount === null || promptCount <= 8 || promptCount >= 128) {
+    if (promptCount === null || promptCount < 8 || promptCount > 128)
+    {
       window.alert('Choose a character count between 8 and 128');
         generatePassword();
     } else {
+
+      // Confirmation for the rest of the parameters selected. //
 
     var confirmSpec = window.confirm('Click OK to confirm special characters');
     var confirmNum = window.confirm('Click OK to confirm numeric characters');
     var confirmLower = window.confirm('Click OK to confirm lowercase characters');
     var confirmUpper = window.confirm('Click OK to confirm uppercase characters');
+
+      // establish empty starting values and convert the prompt count value into an integer. //
+
     var allowed = '';
     var password = '';
     var passwordLength = parseInt(promptCount);
+
+      // check conditions and filter selections. //
+
         {
         if (confirmUpper == true) {
           allowed += upperCase;
@@ -34,7 +48,8 @@ function generatePassword() {
           allowed += charsSymbols;
         }
 
-        //randomizing function to randomize password
+        //for loop for randomizing function to randomize password //
+
         for(var i = 0; i < passwordLength; i++) {
           var random = Math.floor(Math.random() * allowed.length);
           password += allowed[random];
@@ -42,7 +57,7 @@ function generatePassword() {
           passwordText.value = password;
         }};
 }};
-
+        // Call function to write password //
 
 function writePassword() {
   var password = generatePassword();
